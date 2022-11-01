c7768b595c0ffe27e82db14f12135ddaa6d6f4e3
Diana C Peterson <dpeter10@gmail.com>
2022-11-01 09:43:28 -0400

    commit tree c39171478b90e5d1b2b722f08b38ee5cffa8f77a

---
+++ index.js
@@ -0,0 +1,16 @@
+const add = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}
+
+const divide = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 / parameter2;
+}
+
+const square = (parameter1) => {
+    console.log(`Adding ${parameter1}`);
+    return parameter1 * parameter1;
+}
